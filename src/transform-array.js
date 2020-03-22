24dc471a6e2ea7aa0b3e105f5d123c7cac977a4a
inkhl369 <60577224+inkhl369@users.noreply.github.com>
2020-03-22 23:31:32 +0300

    file:add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,31 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
+module.exports = function transform(arr) {
     // remove line with error and write your code here
+    var res = [];
+    if (!Array.isArray(arr)) throw Error();
+     if (arr.length === 0)  return arr;
+      for (var index = 0; index < arr.length; index++) {
+          if (arr[index] === '--discard-next') {
+            if (index + 1 <= arr.length - 1) {
+              index++;
+            }
+          } else 
+          if (arr[index] === '--discard-prev') {
+            if (index - 1 >= 0) {
+              res.pop();
+            }
+          } else 
+          if (arr[index] === '--double-next') {
+            if (index + 1 <= arr.length - 1) {
+              res.push(arr[index + 1]);
+            }
+          } else 
+          if (arr[index] === '--double-prev') {
+            if (index - 1 >= 0) {
+              res.push(arr[index - 1]);
+            }
+          } else {
+          res.push(arr[index]);
+        }
+      }
+      return res;
 };
