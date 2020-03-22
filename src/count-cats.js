43c8f5a6c60b72064185063ad555d48de5d69526
inkhl369 <60577224+inkhl369@users.noreply.github.com>
2020-03-22 21:42:22 +0300

    file:add count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(matrix) {
   // remove line with error and write your code here
-};
+  var i, j, number  = 0;
+  for (i = 0; i < matrix.length; i++) {
+    for (j = 0; j < matrix[i].length; j++) {
+      if(matrix[i][j] == '^^'){
+
+        number++;
+      }
+    }
+  }
+  return number;
+}
