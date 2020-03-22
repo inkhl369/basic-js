73802b55184bc60344658073abecd415e11e55e3
inkhl369 <60577224+inkhl369@users.noreply.github.com>
2020-03-22 22:14:45 +0300

    file:add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,13 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
+module.exports = function dateSample(sampleActivity) {
   // remove line with error and write your code here
+  var number, activ = parseFloat(sampleActivity);
+  
+  if (typeof (activ > 0 && activ < 15 && sampleActivity) === "string" ) {
+  number = Math.ceil(Math.log(MODERN_ACTIVITY / activ) / (0.693 / HALF_LIFE_PERIOD));
+    return number;
+  }
+  return false;
 };
